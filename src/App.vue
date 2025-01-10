b97b846408aea4b0c70a68788ca734b9bf681f61
Matthias Stephan <matthias.stephan@fsbondtec.at>
2025-01-10 10:39:41 +0100

    singlepage

--- src/App.vue
+++ src/App.vue
@@ -21,36 +21,132 @@
         <!-- Main content -->
         <v-main>
             <v-container>
-                <v-tabs v-model="activeTab">
-                    <v-tab>ClassOverview</v-tab>
-                    <v-tab>Performance</v-tab>
-                </v-tabs>
-
-                <v-tabs-window v-model="activeTab">
-                    <v-tabs-window-item>
-                        <ClassOverviewTab />
-                    </v-tabs-window-item>
-                    <v-tabs-window-item>
-                        <PerformanceTab />
-                    </v-tabs-window-item>
-                </v-tabs-window>
+                <apexchart
+                    type="line"
+                    height="350"
+                    :options="chartOptions"
+                    :series="series"
+                ></apexchart>
+                <v-text-field v-model="search" label="Search" class="mb-4"></v-text-field>
+                <v-data-table :headers="headers" :items="filteredItems" class="elevation-1"></v-data-table>
             </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, onMounted, onBeforeUnmount, watch, computed } from "vue";
 import { useTheme } from "vuetify";
 import "@mdi/font/css/materialdesignicons.css";
 
-import ClassOverviewTab from "./components/ClassOverviewTab.vue";
-import PerformanceTab from "./components/PerformanceTab.vue";
-import { incoconnect, incodisconnect } from "./incoconnection.js";
+import { incoconnect, incodisconnect, incoclassmemusage, incousedmem, incototalmem } from "./incoconnection.js";
 
-const activeTab = ref(0);
+let intervalId = null;
+let intervalCounter = 0;
+
+const totalmem = ref(1);
+const membegin = ref({});
 
 const theme = useTheme();
+watch(() => theme.global.current.value.dark,
+    (newVal) => {
+        chartOptions.value = updateChartThemeMode(chartOptions.value, newVal);
+    }
+);
+
+const search = ref("");
+
+const headers = ref([
+    { title: "Class Name", key: "name" },
+    { title: "RAM (Byte)", key: "memory" },
+    { title: "total Δ (Byte)", key: "tdeltab" },
+    { title: "total Δ (%)", key: "tdeltap" },
+    { title: "last Δ (Byte)", key: "ldeltab" },
+    { title: "last Δ (%)", key: "ldeltap" },
+]);
+
+const items = ref([]);
+
+const series = ref([
+    {
+        name: "RAM (Byte)",
+        data: [],
+    },
+]);
+
+const chartOptions = ref({
+    chart: {
+        height: 350,
+        type: "line",
+        toolbar: {
+            show: false,
+        },
+    },
+    title: {
+        text: "RAM Usage",
+        align: "left",
+    },
+    xaxis: {
+        type: 'numeric',
+        range: 60000, // 60 seconds
+        tickAmount: 1, // Only first and last ticks
+        labels: {
+            formatter: function (val, timestamp) {
+                return Math.round((new Date().getTime() - timestamp) / 1000);
+            },
+        },
+    },
+    yaxis: {
+        type: 'numeric',
+        opposite: true,
+        min: 0,
+        max: totalmem,
+        tickAmount: 1, // Only first and last ticks
+    },
+});
+
+const filteredItems = computed(() =>
+  items.value.filter(
+    (item) => item.name.toLowerCase().includes(search.value.toLowerCase())
+  )
+);
+
+const updateMemoryUsage = () => {
+    incousedmem().then((usedmem) => {
+        const currentTime = new Date().getTime();
+        series.value[0].data.push({ x: currentTime, y: usedmem });
+    });
+
+    if (intervalCounter++ % 5 !== 0) {
+        return;
+    }
+
+    incoclassmemusage().then((incoClasses) => {
+        if (incoClasses && typeof incoClasses === "object") {
+            var old = items.value;
+            items.value = [];
+            for (const [classKey, classValue] of Object.entries(incoClasses)) {
+                var firstItem = membegin.value[classKey];
+                var tdeltab = firstItem ? classValue - firstItem : 0;
+                var tdeltap = firstItem ? (tdeltab / firstItem) * 100 : 0;
+                var oldItem = old.find(item => item.name === classKey);
+                var ldeltab = oldItem ? classValue - oldItem.memory : 0;
+                var ldeltap = oldItem ? (ldeltab / oldItem.memory) * 100 : 0;
+                var cla = {
+                    name: classKey,
+                    memory: classValue,
+                    tdeltab: tdeltab,
+                    tdeltap: tdeltap.toFixed(2),
+                    ldeltab: ldeltab,
+                    ldeltap: ldeltap.toFixed(2),
+                };
+                items.value.push(cla);
+            }
+        } else {
+            console.error("Invalid data received from getClassMemUseage");
+        }
+    });
+};
 
 function toggleDarkMode() {
     theme.global.name.value = theme.global.current.value.dark
@@ -72,11 +168,53 @@ function closeWindow() {
 
 onMounted(() => {
     incoconnect("http://192.168.1.251:80");
+    incototalmem().then((total) => {
+        totalmem.value = total;
+    });
+    incoclassmemusage().then((incoClasses) => {
+        for (const [classKey, classValue] of Object.entries(incoClasses)) {
+            membegin.value[classKey] = classValue;
+        }
+    });
+    intervalId = setInterval(updateMemoryUsage, 1000); // Aktualisierung alle 1 Sekunden
 });
 
 onBeforeUnmount(() => {
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
     incodisconnect();
 });
+
+// https://github.com/apexcharts/ng-apexcharts/issues/228#issuecomment-2392632549
+function updateChartThemeMode(chartOptions, darkMode) {
+    const theme = chartOptions.theme;
+    const tooltip = chartOptions.tooltip;
+    const chart = chartOptions.chart;
+
+    // Create a new chart options object to avoid mutating the original object.
+    chartOptions = {
+        ...chartOptions,
+        chart: {
+            ...chart,
+            // Update the foreground color and background color based on the theme mode.
+            foreColor: darkMode ? "#f6f7f8" : "undefined",
+            background: darkMode ? "#424242" : "undefined",
+        },
+        theme: {
+            ...theme,
+            // Update the theme mode.
+            mode: darkMode ? "dark" : "light",
+        },
+        tooltip: {
+            ...tooltip,
+            // Update the tooltip theme based on the theme mode.
+            theme: darkMode ? "dark" : "light",
+        },
+    };
+
+    return chartOptions;
+}
 </script>
 
 <style scoped>
@@ -88,7 +226,7 @@ onBeforeUnmount(() => {
     -webkit-app-region: no-drag; /* Buttons nicht verschiebbar */
 }
 
-.v-tabs-window-item {
+.v-container {
     max-height: calc(100vh - 150px); /* Maximale Höhe des Inhaltsbereichs */
     overflow: auto; /* Scrollbar nur bei Bedarf */
 }
