01ce4c67151add14d4dcb26f47aecf5e300dcd31
Matthias Stephan <matthias.stephan@fsbondtec.at>
2025-01-14 15:54:01 +0100

    app icon

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ const createWindow = () => {
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
+        icon: 'assets/icon.png',
     });
 
     // and load the index.html of the app.
